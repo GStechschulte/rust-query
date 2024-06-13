deb201ed55f072dc406f98bc93987f8febfc6359
GStechschulte <gabriel.stechschulte@hslu.ch>
2024-06-13 16:53:20 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use arrow::datatypes::{Int8Type, Int16Type, Int32Type, Int64Type, Float32Type, Float64Type, Utf8Type};
+
+
+struct ArrowTypes {
+    int8: Int8Type,
+    int16: Int16Type,
+    int32: Int32Type,
+    int64: Int64Type,
+    float32: Float32Type,
+    float64: Float64Type,
+    utf8: Utf8Type,
+}
+
 fn main() {
-    println!("Hello, world!");
+    print!("Arrow Types\n");
 }
